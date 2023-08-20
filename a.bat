135548634ae1fba51d2bc2d34a188361f2c284b5
Your Name <you@example.com>
2023-08-21 01:05:38 +0530

    ss

--- a.bat
+++ a.bat
@@ -1,6 +1,5 @@
-echo "# ali" >> README.md
-git init
-git add *
+  git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
 git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/abedeen/ali.git
